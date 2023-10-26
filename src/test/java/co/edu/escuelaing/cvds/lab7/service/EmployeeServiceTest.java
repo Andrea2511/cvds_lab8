b462aee0c6bac9a639d173aad45fd6d02a74bac0
Andrea Torres <andrea.torres-trmail.escuelaing.edu.co>
2023-10-26 14:05:17 -0500

    lab 8

--- src/test/java/co/edu/escuelaing/cvds/lab7/service/EmployeeServiceTest.java
+++ src/test/java/co/edu/escuelaing/cvds/lab7/service/EmployeeServiceTest.java
@@ -14,7 +14,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 
@@ -32,7 +34,7 @@ class EmployeeServiceTest {
     Entonces la eliminación será exitosa
     """)
     @Test
-    void getPremio() {
+    void deberiaEliminarseCorrectamente() {
         // Arrange / Given - precondition or setup
         Employee mockedEmployee = new Employee(1000858016L, "Carolina", "Medina", "Vendedor", 10.000);
         doNothing().when(mockedEmployeeRepository).deleteById(mockedEmployee.getEmployeeId());
@@ -41,7 +43,6 @@ class EmployeeServiceTest {
         // Assert / Then - verify the output
         verify(mockedEmployeeRepository, times(1)).deleteById(mockedEmployee.getEmployeeId());
     }
-
 
     @Test
     void deberiaEncontrarEmpleadoPorSuId(){
@@ -55,6 +56,79 @@ class EmployeeServiceTest {
 
         //entonces
         assertThat(employees.get(0).getEmployeeId()).isEqualTo(1000858016L);
+    }
+
+    /*
+     * Dado que no hay ningún empleado registrado,
+     * Cuándo lo creo a nivel de servicio,
+     * Entonces la creación será exitosa.
+     */
+    @Test
+    void deberiaCrearseElEmpleadoaNivelDeServicio(){
+
+        //dado
+        Employee mockedEmployee = new Employee(1000858016L, "Carolina", "Medina", "Vendedor", 10.000);
+        List <Employee> mockedEmployees =new ArrayList<>();
+        mockedEmployees.add(0, mockedEmployee);
+
+        // Configura el comportamiento del repositorio Mock para la creación y eliminación de empleados
+        given(mockedEmployeeRepository.findAll()).willReturn(mockedEmployees);
+
+        mockedEmployees = employeeService.getAllEmployees();
+
+        assertTrue(mockedEmployees.get(0).getEmployeeId().equals(mockedEmployee.getEmployeeId()));
+
+    }
+
+    /*
+     * Dado que no hay ningún empleado registrado
+     * Cuándo lo consulto a nivel de servicio
+     * Entonces la consulta no retornará ningún resultado.
+     */
+    @Test
+    void deberiaNoRetornarAlgunResultadoSiNohayUnEmpleadoRegistrado(){
+
+        //dado
+        List <Employee> mockedEmployees =new ArrayList<>();
+
+        given(mockedEmployeeRepository.findAll()).willReturn(mockedEmployees);
+        //cuando
+        mockedEmployees = employeeService.getAllEmployees();
+
+        //entonces
+        assertTrue(mockedEmployees.isEmpty());
+    }
+
+    /*
+     * Dado que tengo 1 empleado registrado,
+     *  Cuándo lo elimino y consulto a nivel de servicio,
+     * Entonces el resultado de la consulta no retornará ningún resultado.
+     *
+     */
+    @Test
+    void deberiaEliminarYNoRetornarResultadosDespues() {
+
+        // Dado que tengo 1 empleado registrado
+        Employee mockedEmployee = new Employee(1000858016L, "Carolina", "Medina", "Vendedor", 10.000);
+
+        // Configura el comportamiento del repositorio Mock para la creación y eliminación de empleados
+        given(mockedEmployeeRepository.save(mockedEmployee)).willReturn(mockedEmployee);
+        willDoNothing().given(mockedEmployeeRepository).deleteById(mockedEmployee.getEmployeeId());
+
+        // Llama al método de servicio para crear un empleado y eliminarlo
+        Employee savedEmployee = employeeService.createEmployee(mockedEmployee);
+
+
+        employeeService.deleteUser(savedEmployee.getEmployeeId());
+
+        // Verifica que el empleado se haya eliminado correctamente
+        verify(mockedEmployeeRepository, times(1)).deleteById(savedEmployee.getEmployeeId());
+
+        // Llama al método de servicio para obtener la lista de empleados después de la eliminación
+        List<Employee> employeesAfterDeletion = employeeService.getAllEmployees();
+
+        // Verifica que la lista esté vacía
+        assertTrue(employeesAfterDeletion.isEmpty());
     }
 
 }
